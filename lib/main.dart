918721bc22a8e2395bc750ba897e06dba044309c
kazi-rayed-hossain <khossain162030@bscse.uiu.ac.bd>
2021-08-23 10:33:56 +0600

    Connecting Functions & Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,34 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  void answerQuestion() {
+    print('ans chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'what\'s your favorite color ?',
+      'what\'s your favorite animal ?'
+    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('My 1st App'),
+        appBar: AppBar(
+          title: const Text('My 1st App'),
         ),
-        body: const Text('this is my default text'),
+        body: Column(
+          children: [
+            const Text('the question!'),
+            // ignore: deprecated_member_use
+            RaisedButton(child: const Text('Answer 1'), onPressed: answerQuestion),
+            // ignore: deprecated_member_use
+            RaisedButton(child: const Text('Answer 2'), onPressed: answerQuestion),
+            // ignore: deprecated_member_use
+            RaisedButton(child: const Text('Answer 3'), onPressed: answerQuestion),
+            // ignore: deprecated_member_use
+            RaisedButton(child: const Text('Answer 4'), onPressed: answerQuestion),
+          ],
+        ),
       ),
     );
   }
